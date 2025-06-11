51f43a3debfbbaa579655458edf7cad3d51fb6f0
Sanjikunnn <fmiqbal66@gmail.com>
2025-06-11 23:03:13 +0700

    updateee PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Neobrutalism',
+        short_name: 'Neobrutalism',
+        description: 'Aplikasi Sosial dengan gaya brutal & keren',
+        theme_color: '#ffffff',
+        background_color: '#0d0d0d',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
